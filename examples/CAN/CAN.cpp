56a69f9db9580ab119fda0ea75c829362a3a6406
LILYGO_L <135582120+Llgok@users.noreply.github.com>
2024-08-26 15:27:48 +0800

    Add CAN testing program

--- examples/CAN/CAN.cpp
+++ examples/CAN/CAN.cpp
@@ -33,7 +33,7 @@ void CAN_Drive_Initialization()
 {
     // Initialize configuration structures using macro initializers
     twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT((gpio_num_t)CAN_TX, (gpio_num_t)CAN_RX, TWAI_MODE_NORMAL);
-    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_1MBITS();
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
     twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
 
     // Install TWAI driver
